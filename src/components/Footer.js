b199811233b8dc2fd6a51a0926301d729092359c
VietKhang <khangviet1996@gmail.com>
2019-05-03 14:20:38 +0700

    upload footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 class Footer extends Component {
   render() {
     return (
@@ -10,55 +10,81 @@ class Footer extends Component {
               <div className="footer-widget about-widget">
                 <h2>About</h2>
                 <p>
-                  Group 4
+                  Sản phẩm được tạo nhằm mục đích học tập. Một số hình ảnh có sử
+                  dụng nguồn ở trên internet, nếu có ảnh nào vi phạm vui lòng
+                  liên hệ qua thông tin bên dưới để gỡ bỏ, cám ơn.
                 </p>
               </div>
             </div>
             <div className="col-lg-3 col-sm-6">
               <div className="footer-widget about-widget">
-                <h2>Page</h2>
+                <h2>Pages</h2>
                 <ul>
                   <li>
-                    <Link to="">Home</Link>
+                    <Link to="/">Trang chủ</Link>
                   </li>
                   <li>
-                    <Link to="">Women</Link>
+                    <Link to="/product/women">Sản phẩm nữ</Link>
                   </li>
                   <li>
-                    <Link to="">Men</Link>
+                    <Link to="/product/men">Sản phẩm nam</Link>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <Link to="/product/sale">Khuyến mãi</Link>
                   </li>
                   <li>
-                    <Link to="">Sales</Link>
-                  </li>
-                  <li>
-                    <Link to="">Brands</Link>
-                  </li>
-                  <li>
-                    <Link to="">Category</Link>
+                    <Link to="/product/brand/sakura">Thương hiệu</Link>
                   </li>
                 </ul>
               </div>
             </div>
             <div className="col-lg-3 col-sm-6">
               <div className="footer-widget contact-widget">
-                <h2>Contact</h2>
+                <h2>Information</h2>
                 <div className="con-info">
-                  <span>C.</span>
-                  <p>Beauty </p>
+                  <span>P.</span>
+                  <p>Beauty Page</p>
                 </div>
                 <div className="con-info">
                   <span>B.</span>
-                  <p>HCMUT</p>
-                </div>
-                <div className="con-info">
-                  <span>T.</span>
-                  <p>+53 345 7953 32453</p>
+                  <p>268 Lý Thường Kiệt, Phường 14, Quận 10, Hồ Chí Minh</p>
                 </div>
                 <div className="con-info">
                   <span>E.</span>
-                  <p>hcmut@edu.vn</p>
+                  <p>khangviet1996@gmail.com</p>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="social-links-warp">
+          <div className="container">
+            <div className="social-links">
+              <a href="" className="instagram">
+                <i className="fa fa-instagram" />
+                <span>instagram</span>
+              </a>
+              <a href="mailto:khangviet1996@gmail.com" className="google-plus">
+                <i className="fa fa-google-plus" />
+                <span>g+plus</span>
+              </a>
+              <a href="https://www.facebook.com/khangviet1996" className="facebook">
+                <i className="fa fa-facebook" />
+                <span>facebook</span>
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=Iz6sUpk_gNU&fbclid=IwAR2Oc6Fr2FfsVQjIxxUjesK8heDjAFF7V91wVTNHvxGdguFui6pfz1xB4fA"
+                className="youtube"
+              >
+                <i className="fa fa-youtube" />
+                <span>youtube</span>
+              </a>
+              <a href="" className="twitter">
+                <i className="fa fa-twitter" />
+                <span>twitter</span>
+              </a>
             </div>
           </div>
         </div>
